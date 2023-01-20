8a7ecde753016ecc598610cbe657f42b6cd77831
Nico Escovilla <35411308+Escovilla@users.noreply.github.com>
2023-01-20 11:06:45 +0800

    Update main.js

--- public/main.js
+++ public/main.js
@@ -375,7 +375,32 @@ $(function() {
       log('attempt to reconnect has failed');
     });
 
-   
+   const publicVapidKey = "BNwhAYBJCkzL10R5odueSiDkMzI8IeNLGp8lnsstedgJyqD1Xt1G5tJScxxSgltB1XIuCCZGAL4aAki0_7o_L7o";
+
+  if('serviceWorker' in navigator) {
+      registerServiceWorker().catch(console.log)
+  }
+
+  async function registerServiceWorker() {
+      const register = await navigator.serviceWorker.register('./worker.js', {
+          scope: '/'
+      });
+
+      const subscription = await register.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: publicVapidKey,
+      });
+
+      await fetch("/subscribe", {
+          method: "POST",
+          body: JSON.stringify(subscription),
+          headers: {
+              "Content-Type": "application/json",
+          }
+      })
+  }
+
+
 
 
 
